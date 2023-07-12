ac8758c07617b580c4a069bee60aea75e5820984
hafizha daffa <hafizhadaffa@gmail.com>
2023-07-12 22:05:59 +0700

    membuat file product pada folder controllers

---
+++ controllers/Product.js
@@ -0,0 +1,27 @@
+import Products from "../models/ProductModel.js";
+
+export const getProducts = (req, res) =>{
+
+
+}
+
+export const getProductsById = (req, res) =>{
+
+
+}
+
+export const createProducts = (req, res) =>{
+
+
+}
+
+export const updateProducts = (req, res) =>{
+
+
+}
+
+export const deleteProducts = (req, res) =>{
+
+
+}
+
